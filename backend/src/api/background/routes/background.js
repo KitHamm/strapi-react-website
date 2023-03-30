ca0653b77601857d2f67241b72de3f3fcdc3c989
Chris Perry <cmperry@live.co.uk>
2023-03-30 13:50:14 +0100

    removed depricated database assets

--- backend/src/api/background/routes/background.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * background router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::background.background');
--- backend/src/api/background/services/background.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * background service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::background.background');
